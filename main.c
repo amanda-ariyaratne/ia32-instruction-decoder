a72c37d683a0f8bb321c2d3906740bb5359e3807
Amanda Ariyaratne <amanda.ariyaratne@protonmail.com>
2021-08-24 17:38:56 +0530

    fix decode function

--- main.c
+++ main.c
@@ -3,24 +3,30 @@
 
 #include "decoder.c"
 
+int convertHexToInt(char c);
+
 int main() {
 
     FILE *fptr;
 
     fptr = fopen("sample.txt", "r");
-    char *instructions = malloc(1000 * sizeof(char));
+    int *instructions = malloc(1000 * sizeof(int));
     int len = 0;
-    int c;
+    char c;
 
     if (fptr == NULL)
     {
-        printf("The file does not exist.");
+        printf("The file does not exist. Exit Program.");
         exit(1);
     }
 
     while ((c = fgetc(fptr)) != EOF)
     {
-        instructions[len++] = (char) c;
+        if (c == ' ')
+        {
+            continue;
+        }
+        instructions[len++] = convertHexToInt(c);
     }
     instructions[len] = '\0';
     fclose(fptr);
@@ -35,4 +41,52 @@ int main() {
     free(instructions);
 
     return 0;
+}
+
+int convertHexToInt(char c)
+{
+    switch(c)
+    {
+        case '0':
+            return 0;
+        case '1':
+            return 1;
+        case '2':
+            return 2;
+        case '3':
+            return 3;
+        case '4':
+            return 4;
+        case '5':
+            return 5;
+        case '6':
+            return 6;
+        case '7':
+            return 7;
+        case '8':
+            return 8;
+        case '9':
+            return 9;
+        case 'a':
+        case 'A':
+            return 10;
+        case 'b':
+        case 'B':
+            return 11;
+        case 'c':
+        case 'C':
+            return 12;
+        case 'd':
+        case 'D':
+            return 13;
+        case 'e':
+        case 'E':
+            return 14;
+        case 'f':
+        case 'F':
+            return 15;
+        default:
+            printf("Invalid input. Exit Program.");
+            exit(1);
+    }
 }
