0b6ace95100ca6e8ae2237a95aa9e962f5deade1
MadhaviG97 <madhavi.17@cse.mrt.ac.lk>
2021-09-10 18:22:04 +0530

    dump messages

--- instruction_impl/add.c
+++ instruction_impl/add.c
@@ -162,17 +162,19 @@ void add_immd_to_al_8bit(int mod, int reg, int rm, int scale, int index, int bas
     unsigned int val = op1 + op2;
 
     reg_store(0, w_bit, val);
-    printf("expect %d + %d = %d output %d\n", op1, op2, val, reg_load(0, w_bit));
+    printf("expect %d + %d = %d\n", op1, op2, val);
 }
 
 void add_immd_to_eax_32bit(int mod, int reg, int rm, int scale, int index, int base, unsigned int dis, unsigned int immd)
 {
+    printf("INS: Add imm32 to EAX\n");
     int w_bit = 1;
     unsigned int op1 = reg_load(0, w_bit);   // TODO: remove hardcode val of register 0
     unsigned int op2 = immd;
 
     unsigned int val = op1 + op2;
     reg_store(0, w_bit, val);
+    printf("expect %u + %u = %u\n", op1, op2, val);
 }
 
 void add_immd_to_memory_8bit(int mod, int reg, int rm, int scale, int index, int base, unsigned int dis, unsigned int immd)
