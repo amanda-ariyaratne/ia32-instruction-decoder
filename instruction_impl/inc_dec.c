b4c9ddfd43b794251ee5b2839db1a9fe5a54eeaf
MadhaviG97 <madhavi.17@cse.mrt.ac.lk>
2021-10-12 18:30:29 +0530

    inc dec put into one opcode

--- instruction_impl/inc_dec.c
+++ instruction_impl/inc_dec.c
@@ -4,29 +4,44 @@
 #include "../memory.c"
 #include "../decoder2.c"
 
-#ifndef INC
-#define INC
+#ifndef INC_DEC
+#define INC_DEC
+
+unsigned int inc_or_dec;
 
 void setAddFlags(unsigned int op1, unsigned int op2, unsigned int val, int w_bit);
 
-void inc_reg_8bit(mod, reg, rm, scale, index, base, dis, immd)
+void inc_dec_rm_8bit(int mod, int reg, int rm, int scale, int index, int base, unsigned int dis, unsigned int immd)
 {
-    printf("INS: Inc reg 8bit\n");
+    if (reg == 0)
+        inc_or_dec = 0x01;
+    else
+        inc_or_dec = 0xff;
+
+    if (mod == 3)
+        inc_dec_reg_8bit(mod, reg, rm, scale, index, base, dis, immd);
+    else
+        inc_dec_mem_8bit(mod, reg, rm, scale, index, base, dis, immd);
+}
+
+void inc_dec_reg_8bit(mod, reg, rm, scale, index, base, dis, immd)
+{
+    printf("INS: Inc_dec reg 8bit\n");
     int w_bit = 0;
     unsigned int op1 = reg_load(rm, w_bit);
-    unsigned int op2 = 1;
+    unsigned int op2 = inc_or_dec;
     unsigned int val = op1 + op2;
 
     setAddFlags(op1, op2, val, w_bit);
     reg_store(rm, w_bit, val);
 }
 
-void inc_mem_8bit(mod, reg, rm, scale, index, base, dis, immd)
+void inc_dec_mem_8bit(mod, reg, rm, scale, index, base, dis, immd)
 {
     int w_bit = 0;
     unsigned int addr = getEffectiveAddressFromModRM(mod, rm, scale, index, base, dis);
     unsigned int op1 = mem_load(addr, w_bit);
-    unsigned int op2 = 1;
+    unsigned int op2 = inc_or_dec;
 
     unsigned int val = op1 + op2;
 
@@ -34,32 +49,37 @@ void inc_mem_8bit(mod, reg, rm, scale, index, base, dis, immd)
     mem_store(addr, w_bit, val);
 }
 
-void inc_rm_8bit(int mod, int reg, int rm, int scale, int index, int base, unsigned int dis, unsigned int immd)
+void inc_dec_rm_32bit(int mod, int reg, int rm, int scale, int index, int base, unsigned int dis, unsigned int immd)
 {
-    if (mod == 3)
-        inc_reg_8bit(mod, reg, rm, scale, index, base, dis, immd);
+    if (reg == 0)
+        inc_or_dec = 0x0001;
     else
-        inc_mem_8bit(mod, reg, rm, scale, index, base, dis, immd);
+        inc_or_dec = 0xffff;
+        
+    if (mod == 3)
+        inc_dec_reg_32bit(mod, reg, rm, scale, index, base, dis, immd);
+    else
+        inc_dec_mem_32bit(mod, reg, rm, scale, index, base, dis, immd);
 }
 
-void inc_reg_32bit(mod, reg, rm, scale, index, base, dis, immd)
+void inc_dec_reg_32bit(mod, reg, rm, scale, index, base, dis, immd)
 {
-    printf("INS: Inc reg 8bit\n");
+    printf("INS: Inc_dec reg 8bit\n");
     int w_bit = 1;
     unsigned int op1 = reg_load(rm, w_bit);
-    unsigned int op2 = 1;
+    unsigned int op2 = inc_or_dec;
     unsigned int val = op1 + op2;
 
     setAddFlags(op1, op2, val, w_bit);
     reg_store(rm, w_bit, val);
 }
 
-void inc_mem_32bit(mod, reg, rm, scale, index, base, dis, immd)
+void inc_dec_mem_32bit(mod, reg, rm, scale, index, base, dis, immd)
 {
     int w_bit = 1;
     unsigned int addr = getEffectiveAddressFromModRM(mod, rm, scale, index, base, dis);
     unsigned int op1 = mem_load(addr, w_bit);
-    unsigned int op2 = 1;
+    unsigned int op2 = inc_or_dec;
 
     unsigned int val = op1 + op2;
 
@@ -67,17 +87,9 @@ void inc_mem_32bit(mod, reg, rm, scale, index, base, dis, immd)
     mem_store(addr, w_bit, val);
 }
 
-void inc_rm_32bit(int mod, int reg, int rm, int scale, int index, int base, unsigned int dis, unsigned int immd)
-{
-    if (mod == 3)
-        inc_reg_32bit(mod, reg, rm, scale, index, base, dis, immd);
-    else
-        inc_mem_32bit(mod, reg, rm, scale, index, base, dis, immd);
-}
-
-// void inc_reg_8bit(int mod, int reg, int rm, int scale, int index, int base, unsigned int dis, unsigned int immd)
+// void inc_dec_reg_8bit(int mod, int reg, int rm, int scale, int index, int base, unsigned int dis, unsigned int immd)
 // {
-//     printf("INS: Inc reg 8bit\n");
+//     printf("INS: Inc_dec reg 8bit\n");
 //     reg = 0;
 //     int w_bit = 0;
     
