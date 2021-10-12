694b020cc19dfd5417f8d62ab1726161ac5cf63d
Amanda Ariyaratne <amanda.ariyaratne@protonmail.com>
2021-10-12 17:03:14 +0530

    add CMP instruction

--- instruction_impl/imm_grp_1A.c
+++ instruction_impl/imm_grp_1A.c
@@ -3,32 +3,39 @@
 #include "../decoder2.c"
 #include "add.c"
 #include "and.c"
+#include "cmp.c"
 
 #ifndef GROUP_1A
 #define GROUP_1A
 
 void immd_to_reg_8bit(int mod, int reg, int rm, int scale, int index, int base, unsigned int dis, unsigned int immd)
 {
-    if (rm == 0)
+    if (reg == 0)
         add_immd_to_reg_8bit(mod, reg, rm, scale, index, base, dis, immd);
-    else if (rm == 4)
+    else if (reg == 4)
         and_immd_to_reg_8bit(mod, reg, rm, scale, index, base, dis, immd);
+    else if (reg == 7)
+        cmp_immd_with_rm_8bit(mod, reg, rm, scale, index, base, dis, immd);
 }
 
 void immd_to_reg_32bit(int mod, int reg, int rm, int scale, int index, int base, unsigned int dis, unsigned int immd)
 {
-    if (rm == 0)
+    if (reg == 0)
         add_immd_to_reg_32bit(mod, reg, rm, scale, index, base, dis, immd);
-    else if (rm == 4)
+    else if (reg == 4)
         and_immd_to_reg_32bit(mod, reg, rm, scale, index, base, dis, immd);
+    else if (reg == 7)
+        cmp_immd_with_rm_32bit(mod, reg, rm, scale, index, base, dis, immd);
 }
 
 void immd_to_reg_32bit_sign_extended(int mod, int reg, int rm, int scale, int index, int base, unsigned int dis, unsigned int immd)
 {
-    if (rm == 0)
+    if (reg == 0)
         add_immd_to_reg_32bit_sign_extended(mod, reg, rm, scale, index, base, dis, immd);
-    else if (rm == 4)
+    else if (reg == 4)
         and_immd_to_reg_32bit_sign_extended(mod, reg, rm, scale, index, base, dis, immd);
+    else if (reg == 7)
+        cmp_immd_with_rm_32bit_sign_extended(mod, reg, rm, scale, index, base, dis, immd);
 }
 
 #endif
--- opcode_table.c
+++ opcode_table.c
@@ -100,7 +100,7 @@ void init_one_byte_opcode_map(){
     one_byte_opcode_map[8][13][0].modRM = true;
     one_byte_opcode_map[8][13][0].instruction = &store_eff_addr_from_reg_32bit;
 
-    // mov
+    // MOV
     one_byte_opcode_map[8][8][0].immdSize = 0;
     one_byte_opcode_map[8][8][0].modRM = true;
     one_byte_opcode_map[8][8][0].instruction = &mov_reg1_to_reg2_8bit;
@@ -117,7 +117,7 @@ void init_one_byte_opcode_map(){
     one_byte_opcode_map[8][11][0].modRM = true;
     one_byte_opcode_map[8][11][0].instruction = &mov_rm_to_reg_32bit;
 
-    // immediate
+    // MOV immediate
     one_byte_opcode_map[12][6][0].immdSize = 1;
     one_byte_opcode_map[12][6][0].modRM = true;
     one_byte_opcode_map[12][6][0].instruction = &mov_immd_to_rm_8bit;
@@ -126,7 +126,7 @@ void init_one_byte_opcode_map(){
     one_byte_opcode_map[12][7][0].modRM = true;
     one_byte_opcode_map[12][7][0].instruction = &mov_immd_to_rm_32bit;
 
-    // 0 -> 7 = immd8
+    // MOV 0 -> 7 = immd8
     int i;
     for (i = 0; i < 8; i++)
     {
@@ -134,7 +134,7 @@ void init_one_byte_opcode_map(){
         one_byte_opcode_map[11][i][0].modRM = false;
         one_byte_opcode_map[11][i][0].instruction = &mov_immd_to_reg_8bit;
     }
-    // 8 -> 15 = imm32
+    // MOV 8 -> 15 = imm32
     for (i = 8; i < 16; i++)
     {
         one_byte_opcode_map[11][i][0].immdSize = 4;
@@ -168,10 +168,37 @@ void init_one_byte_opcode_map(){
     one_byte_opcode_map[3][13][0].modRM = false;
     one_byte_opcode_map[3][13][0].instruction = &cmp_immd_with_eax_32bit;
 
-        // one_byte_opcode_map[][][0].immdSize = ;
+    one_byte_opcode_map[8][0][0].immdSize = 1;
+    one_byte_opcode_map[8][0][0].modRM = true;
+    one_byte_opcode_map[8][0][0].instruction = &immd_to_reg_8bit;
+
+    one_byte_opcode_map[8][1][0].immdSize = 4;
+    one_byte_opcode_map[8][1][0].modRM = true;
+    one_byte_opcode_map[8][1][0].instruction = &immd_to_reg_32bit;
+
+    one_byte_opcode_map[8][3][0].immdSize = 1;
+    one_byte_opcode_map[8][3][0].modRM = true;
+    one_byte_opcode_map[8][3][0].instruction = &immd_to_reg_32bit_sign_extended;
+
+    one_byte_opcode_map[3][8][0].immdSize = 0;
+    one_byte_opcode_map[3][8][0].modRM = true;
+    one_byte_opcode_map[3][8][0].instruction = &cmp_reg_with_rm_8bit;
+    
+    one_byte_opcode_map[3][9][0].immdSize = 0;
+    one_byte_opcode_map[3][9][0].modRM = true;
+    one_byte_opcode_map[3][9][0].instruction = &cmp_reg_with_rm_32bit;
+
+    one_byte_opcode_map[3][10][0].immdSize = 0;
+    one_byte_opcode_map[3][10][0].modRM = true;
+    one_byte_opcode_map[3][10][0].instruction = &cmp_rm_with_reg_8bit;
+
+    one_byte_opcode_map[3][11][0].immdSize = 0;
+    one_byte_opcode_map[3][11][0].modRM = true;
+    one_byte_opcode_map[3][11][0].instruction = &cmp_rm_with_reg_32bit;
+
+    // one_byte_opcode_map[][][0].immdSize = ;
     // one_byte_opcode_map[][][0].modRM = ;
     // one_byte_opcode_map[][][0].instruction = &;
-    
 }
 
 #endif
